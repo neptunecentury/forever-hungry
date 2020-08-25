9c4d51dca91a8d33e9272a30812970b6353dc3ea
SFort <github@ssf.tf>
2020-08-25 15:15:23 +0200

    Added configs

---
+++ src/main/java/sf/ssf/sfort/Eats.java
@@ -0,0 +1,56 @@
+package sf.ssf.sfort;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.*;
+
+public class Eats implements ModInitializer{
+    public static int delay = 65;
+    public static boolean forceDelay = false;
+    public static int difficultyScale = 0;
+    
+    @Override
+    public void onInitialize() {
+        // Configs
+        File confFile = new File(
+                FabricLoader.getInstance().getConfigDir().toString(),
+                "EternalEats.conf"
+        );
+        try {
+            confFile.createNewFile();
+            List<String> la = Files.readAllLines(confFile.toPath());
+            List<String> defaultDesc = Arrays.asList(
+                    "^-Duration in ticks between eating attempts when full [65]",
+                    "^-By how much should cooldown scale with difficulty [0] // cooldown+(cooldown*difficulty*scale)",
+                    "^-Force cooldown even if not full [false] true | false"
+            );
+            String[] init =new String[Math.max(la.size(), defaultDesc.size() * 2)];
+            Arrays.fill(init,"");
+            String[] ls = la.toArray(init);
+            for (int i = 0; i<defaultDesc.size();++i)
+                ls[i*2+1]= defaultDesc.get(i);
+
+            try{ delay =Math.max(Integer.parseInt(ls[0]),0);}catch (NumberFormatException ignored){}
+            ls[0] = String.valueOf(delay);
+
+            try{ difficultyScale = Math.max(Integer.parseInt(ls[2]),0);}catch (NumberFormatException ignored){}
+            ls[2] = String.valueOf(difficultyScale);
+
+            forceDelay = ls[4].contains("true");
+            ls[4] = String.valueOf(forceDelay);
+
+            if(ls.length>defaultDesc.size()*2){
+                for (int i = (defaultDesc.size()*2)+1; i<=ls.length;i+=2){
+                    ls[i] = "!#Unknown value / config from the future";
+                }
+            }
+            Files.write(confFile.toPath(), Arrays.asList(ls));
+            System.out.println("tf.ssf.sfort.eternaleats successfully loaded config file");
+        } catch(Exception e) {
+            System.out.println("tf.ssf.sfort.eternaleats failed to load config file, using defaults\n"+e);
+        }
+    }
+}
--- src/main/java/sf/ssf/sfort/mixin/Eats.java
+++ src/main/java/sf/ssf/sfort/mixin/Eats.java
@@ -1,29 +1,41 @@
 package sf.ssf.sfort.mixin;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.player.HungerManager;
 import net.minecraft.entity.player.PlayerAbilities;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Arm;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Surrogate;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.Random;
 
 @Mixin(PlayerEntity.class)
-public class Eats{
+public class Eats extends LivingEntity {
 	@Dynamic
-	byte foodDelay = 0;
+	int foodDelay = 0;
 	@Shadow
 	protected HungerManager hungerManager = new HungerManager();
+
+	protected Eats(EntityType<? extends LivingEntity> entityType, World world) {
+		super(entityType, world);
+	}
+
 	@Inject(method = "tick", at = @At("HEAD"))
 	public void tick(CallbackInfo info) {
 		if (foodDelay > 0) {
@@ -32,10 +44,20 @@ public class Eats{
 	}
 	@Inject(method = "canConsume", at = @At("HEAD"), cancellable = true)
 	public void canConsume(boolean ignoreHunger, CallbackInfoReturnable info) {
-		if (foodDelay==0 && !hungerManager.isNotFull()){
+		if ((!hungerManager.isNotFull() || sf.ssf.sfort.Eats.forceDelay) && foodDelay==0){
 			info.setReturnValue(true);
-			foodDelay = 65;
+			foodDelay = sf.ssf.sfort.Eats.delay;
+			foodDelay += foodDelay*sf.ssf.sfort.Eats.difficultyScale*this.world.getDifficulty().getId();
 			info.cancel();
 		}
 	}
+
+	@Surrogate
+	public Iterable<ItemStack> getArmorItems() { return null; }
+	@Surrogate
+	public ItemStack getEquippedStack(EquipmentSlot slot) { return null; }
+	@Surrogate
+	public void equipStack(EquipmentSlot slot, ItemStack stack) {}
+	@Surrogate
+	public Arm getMainArm() { return null; }
 }
