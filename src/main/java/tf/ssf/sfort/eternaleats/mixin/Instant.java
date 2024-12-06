5f6e9bca676ed98daae54607d71432c4ee6d4297
SFort <github@ssf.tf>
2024-12-06 14:40:11 +0100

    Fix #1

--- src/main/java/tf/ssf/sfort/eternaleats/mixin/Instant.java
+++ src/main/java/tf/ssf/sfort/eternaleats/mixin/Instant.java
@@ -1,5 +1,8 @@
 package tf.ssf.sfort.eternaleats.mixin;
 
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,8 +14,9 @@ import java.util.Objects;
 
 @Mixin(Item.class)
 public class Instant {
-	@Inject(method="getMaxUseTime(Lnet/minecraft/item/ItemStack;)I", at=@At(value="HEAD"), cancellable = true)
-	private void getMaxUseTime(ItemStack stack, CallbackInfoReturnable<Integer> info) {
-		if (stack.getItem().isFood()) info.setReturnValue(Objects.requireNonNull(stack.getItem().getFoodComponent()).isSnack()? Config.eatDurationY : Config.eatDurationX);
+	@Inject(method="getMaxUseTime(Lnet/minecraft/item/ItemStack;Lnet/minecraft/entity/LivingEntity;)I", at=@At(value="HEAD"), cancellable = true)
+	private void getMaxUseTime(ItemStack stack, LivingEntity entity, CallbackInfoReturnable<Integer> info) {
+		FoodComponent foodComponent = (FoodComponent)stack.get(DataComponentTypes.FOOD);
+		if (foodComponent != null) info.setReturnValue(foodComponent.getEatTicks() <= 1.55f ? Config.eatDurationY : Config.eatDurationX);
 	}
 }
